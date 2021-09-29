ef27c2a03bb24769388ea18629fe8e182274172b
Dávid Lajos Mihalovits <dmihalovits@gmail.com>
2021-09-29 09:45:56 +0200

    netlify functions

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,3 +1,6 @@
 exports.handler = async function (event, context) {
-    console.log("hello there");
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" }),
+    };
 };
